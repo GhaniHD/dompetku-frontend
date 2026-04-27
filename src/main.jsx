1610ef6fceab967971d29d8c17af87bf51968a62
GhaniHD <120998349+GhaniHD@users.noreply.github.com>
2026-04-27 15:54:02 +0700

    feat: add service worker for PWA

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
+// Registrasi Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
